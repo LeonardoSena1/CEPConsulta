1535164e1b61a8f8e789f1704e3a7dc2154e7f68
LeonardoSena1 <57286132+LeonardoSena1@users.noreply.github.com>
2020-03-18 10:30:17 -0300

    Primeito Commit

---
+++ ConsultaCep/ICepApiServece.cs
@@ -0,0 +1,12 @@
+﻿using Refit;
+using System.Threading.Tasks;
+
+namespace ConsultaCep
+{
+    public interface ICepApiServece
+    {
+        [Get("/ws/{cep}/json")]
+        Task<CepResponse> GetAddressAsync(string cep);
+
+    }
+}
---
+++ ConsultaCep/Program.cs
@@ -0,0 +1,37 @@
+﻿using Refit;
+using System;
+using System.Threading.Tasks;
+
+namespace ConsultaCep
+{
+    class Program
+    {
+        class MainClass
+        {
+            static async Task Main(string[] args)
+            {
+                try
+                {
+                    var cepClient = RestService.For<ICepApiServece>("https://viacep.com.br");
+                    Console.Write("Informe seu cep: ");
+
+                    string cepInformado = Console.ReadLine().ToString();
+                    Console.WriteLine("Consultado informações do CEP: " + cepInformado);
+
+                    var address = await cepClient.GetAddressAsync(cepInformado);
+
+                    Console.Write($"\nRua: {address.Logradouro}\nBairro: {address.Bairro}\nComplemento: {address.Complemento}\nCidade: {address.Localidade}\nuf: {address.Uf} ");
+
+                    Console.ReadKey();
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Erro na consulta de Cep: " + e.Message);
+                    Console.WriteLine("Informe o Cep correto.");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
